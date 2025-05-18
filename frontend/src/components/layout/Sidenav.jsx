60d247dd161ef79b93f4cb0d0db69f707de52ba1
Aceszu <acekiller4523@gmail.com>
2025-05-19 00:59:13 +0800

    Added first components to write

---
+++ frontend/src/components/layout/Sidenav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Sidenav() {
+    return (
+        //TODO: Create a sidenav, install tailwindcss, figure out which step is start first like
+        //Writing components first or follow tutorial
+        <>
+        </>
+    )
+}
